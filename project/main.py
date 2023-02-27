307295cffb58be4145806981209ee040bb3c41ad
Вячеслав Петров <213xixixi@gmail.com>
2023-02-27 21:23:47 +0300

    докстринги добавил

--- project/main.py
+++ project/main.py
@@ -13,6 +13,9 @@ countt = 0
 
 
 def hello():
+    """Приветственная функция,
+    которая предлагает участнику запустить викторину"""
+    print('Привет, это викторина. Играем?')
     while True:
         agree = input('Да / Нет \n')
         if agree == 'Да' or agree == 'Нет':
@@ -27,6 +30,8 @@ def hello():
 
 
 def check_answer(response):
+    """Функция, проверяющая правильность ответа.
+    Если ответ верный, в словарь добавляется плюсик."""
     for key, value in correct_answers.items():
         if response.lower() == value:
             correct_answers.pop(key)
@@ -34,11 +39,13 @@ def check_answer(response):
             return random.choice(right)
         else:
             correct_answers.pop(key)
-            lst_count.append('-')
             return random.choice(wrong)
 
 
 def check_input(response):
+    """Функция, проверяющая корректность ввода ответа на вопрос.
+    В зависимости от ввода выкидывает True,
+    если ввод неверный и False, если верный."""
     splitted = ''.join(response.lower().split(','))
     for symbol in splitted:
         if symbol in complex:
@@ -47,14 +54,19 @@ def check_input(response):
             return False
 
 
-def count(listik, counttt):
+def counter(listik, counttt):
+    """Функция подсчета набранных баллов.
+    Обращается к списку и подсчитывает кол-во плюсиков,
+    добавленных функцией check_answer()."""
     for i in listik:
         if i == '+':
             counttt += 1
     print(f'Количество баллов: {counttt}.')
 
 
-def main():
+def questions_and_answers():
+    """Функция, выполняющая полный цикл процедур 'вопрос' - 'ответ'.
+    Когда вопросы заканчиваются, выводит завершительное сообщение."""
     while True:
         if __name__ == '__main__':
             for key, value in dictionary.items():
@@ -78,8 +90,12 @@ def main():
                 break
 
 
-print('Привет, это викторина. Играем?')
-print(hello())
-time.sleep(TIMEOUT)
+def main():
+    """Основная функция, объединяющая работу предыдущих."""
+    hello()
+    time.sleep(TIMEOUT)
+    questions_and_answers()
+    counter(lst_count, countt)
+
+
 main()
-print(count(lst_count, countt))
